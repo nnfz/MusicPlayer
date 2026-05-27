e7b88208c704776cb9ab40754260dd071ae9bae7
nnfzhomepc <mr.botnikoff@ya.ru>
2026-05-27 09:19:13 +0500

    pizdec

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,5 +1,6 @@
 #include "MusicPlayer.h"
 #include <QApplication>
+#include <QFontDatabase>
 
 int main(int argc, char *argv[])
 {
@@ -24,6 +25,25 @@ int main(int argc, char *argv[])
     
     app.setPalette(darkPalette);
     
+    QFontDatabase::addApplicationFont(":/fonts/NeueMontreal-Bold.ttf");
+    QFontDatabase::addApplicationFont(":/fonts/NeueMontreal-BoldItalic.ttf");
+    QFontDatabase::addApplicationFont(":/fonts/NeueMontreal-Italic.ttf");
+    QFontDatabase::addApplicationFont(":/fonts/NeueMontreal-Light.ttf");
+    QFontDatabase::addApplicationFont(":/fonts/NeueMontreal-LightItalic.ttf");
+    QFontDatabase::addApplicationFont(":/fonts/NeueMontreal-Medium.ttf");
+    QFontDatabase::addApplicationFont(":/fonts/NeueMontreal-MediumItalic.ttf");
+    int fontId = QFontDatabase::addApplicationFont(":/fonts/NeueMontreal-Regular.ttf");
+    
+    if (fontId != -1) {
+        QStringList families = QFontDatabase::applicationFontFamilies(fontId);
+        if (!families.isEmpty()) {
+            QFont appFont(families.at(0));
+            appFont.setStyleStrategy(QFont::PreferAntialias);
+            appFont.setPointSize(10);
+            app.setFont(appFont);
+        }
+    }
+
     MusicPlayer player;
     player.show();
     
